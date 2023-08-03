b98cbfa0dbfb18e31533b520e1f3f946441fe63d
George Hlongwane <georgehlongwane8@gmail.com>
2023-08-03 05:15:52 +0200

    Completed Day 6 and modified the script for Day 5

--- Day 05 - Blurry Loading/js/script.js
+++ Day 05 - Blurry Loading/js/script.js
@@ -17,7 +17,7 @@ function blurring() {
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-// https://stackoverflow.com/questions/10756313/javascript-query-map-a-range-of-numbers-to-another-range-of-numbers
+// The blur effect and speed up effect was taken from code available on Stack Overflow ->  https://stackoverflow.com/questions/10756313/javascript-query-map-a-range-of-numbers-to-another-range-of-numbers
 
 const scale =(num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
